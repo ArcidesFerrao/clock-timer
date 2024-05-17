34b597ba1bab615c66462a942bb13c92cd467bd4
ArcidesFerrao <cidesferrao@gmail.com>
2024-05-17 11:14:45 +0200

    resolve: timer delay + cleaup code

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import TimerDisplay from './TimerDisplay';
 import { Icon } from "@iconify-icon/react";
 
@@ -7,27 +7,20 @@ import { Icon } from "@iconify-icon/react";
 export default function Timer() {
 
     const [starter, setStarter] = useState(true);
-    const [entry, setEntry] = useState("");
     const [option, setOption] = useState(60);    
-    const [timestamp, setTimestamp] = useState();
-    // setTimestamp(new Date());
     
     const handleStarter = () => {
         setStarter(!starter);
-        setEntry("");
-        
     }
     
     const handleOne = () => {
         const timeS = new Date();
-        // event.preventDefault();
         setOption(timeS.setSeconds(timeS.getSeconds() + 60));
        
         setStarter(!starter);
     }
     const handleFive = () => {
         const timeS = new Date();
-        // event.preventDefault();
         setOption(timeS.setSeconds(timeS.getSeconds() + 300));
        
         setStarter(!starter);
@@ -35,7 +28,6 @@ export default function Timer() {
 
     const handleMax = () => {
         const timeS = new Date();
-        // event.preventDefault();
         setOption(timeS.setSeconds(timeS.getSeconds() + 900));
        
         setStarter(!starter);
@@ -43,21 +35,24 @@ export default function Timer() {
 
   return (
     <div className='timer-app'>
-
+      <div className="timer-title">
+        <h2>Timer</h2>
+      </div>
         { 
           starter && starter ? 
           <>
+
             <div className='option-btns' >
-                <button onClick={handleOne} >1 min</button>
-                <button onClick={handleFive} >5 min</button>
-                <button onClick={handleMax}  >15 min</button>
+                <button className='start-btn'  onClick={handleOne} >1 min</button>
+                <button className='start-btn' onClick={handleFive} >5 min</button>
+                <button className='start-btn' onClick={handleMax}  >15 min</button>
             </div>
           </>
             :
-            <>
-            <TimerDisplay expiryTimestamp={option} />
-            <button className='set-back' onClick={handleStarter} ><Icon icon="ep:back" width="1rem" height="1rem" /></button>
-            </>
+            <div className='dis-timer'>
+              <TimerDisplay expiryTimestamp={option} />
+              <button className='set-back' onClick={handleStarter} ><Icon icon="ep:back" width="1rem" height="1rem" /></button>
+            </div>
         }
     </div>
   )
